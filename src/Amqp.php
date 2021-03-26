cb0e9fd3e09234049df13243071597237a2618d3
LuRa <luca.rasconi@zappyrent.com>
2021-03-26 17:24:36 +0100

    removed typo

--- src/Amqp.php
+++ src/Amqp.php
@@ -108,7 +108,7 @@ class Amqp
             ->setup();
 
         $consumer->consume($queue, $callback);
-        Request::shutdown($consumer->getChannel(), $consumer->getConnection());;
+        Request::shutdown($consumer->getChannel(), $consumer->getConnection());
 	    return $consumer;
     }
 
